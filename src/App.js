63e11547d7e98088cda7b834584e6b6464d00582
Daniel Angulo <dangulo4@gmail.com>
2020-05-12 22:53:10 -0700

    api call into app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import FriendCard from './components/EmployeeCard';
+import EmployeeCard from './components/EmployeeCard';
 import Wrapper from './components/Wrapper';
-// import friends from './friends.json';
+import API from './utils/API';
 import './App.css';
 
 class App extends React.Component {
-  state = {
-    employees,
+  state = { employees: [] };
+
+  componentDidMount() {
+    this.getEmployees();
+  }
+
+  getEmployees = (query) => {
+    API.search(query)
+      .then((res) => this.setState({ result: res.data }))
+      .catch((err) => console.log(err));
   };
 
   render() {
     return (
       <Wrapper>
-        {/* <Title>Friends List</Title> */}
-        {this.state.friends.map((friend) => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+        {/* Looping through all the users from the API  */}
+        {[...this.state.result].map((item) => (
+          <EmployeeCard
+            picture={item.picture}
+            firstName={item.firstName}
+            lastName={item.lastName}
+            email={item.email}
+            phone={item.phone}
+            key={item.key}
           />
         ))}
       </Wrapper>
